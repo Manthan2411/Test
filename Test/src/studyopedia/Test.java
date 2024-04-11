ce91f76844441b5587c711f862aab46b7514176b
Manthan2411 <162007067+Manthan2411@users.noreply.github.com>
2024-04-11 16:35:52 +0530

    initial commit.

---
+++ Test/src/studyopedia/Test.java
@@ -0,0 +1,20 @@
+package studyopedia;
+public class Test 
+{
+	public static void main(String[] args) 
+	{
+	int year=2027;
+		if((year%4==0)|| (year%4==0 && year%100!=0))
+		{
+			System.out.println("Year is leap year");
+		}
+		else
+		{
+			System.out.println("Year is not leap year");
+		}
+		
+		int n=3;
+		char[][] board=new char[3][3];
+	}
+
+}
